f267c9058fc3b8f6329e90896ce657553f664818
mylesharrison <mylesharrison2002@gmail.com>
2026-05-29 23:46:05 -0500

    Read data from esp32

--- dashboard.py
+++ dashboard.py
@@ -1,1 +1,12 @@
-print("Hello, World!")
+import serial
+PORT = "COM3"
+BAUD = 115200
+
+with serial.Serial(PORT, BAUD, timeout=1) as esp32:
+    print(f"Connected to {PORT}. Press Ctrl+C to stop.")
+
+    while True:
+        line = esp32.readline().decode("utf-8", errors="ignore").strip()
+                                       
+        if line:
+            print(line)
