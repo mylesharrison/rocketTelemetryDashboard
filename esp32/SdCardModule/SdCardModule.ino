045ff2156e096a509199fdd4046ff559ebd6759a
mylesharrison <mylesharrison2002@gmail.com>
2026-06-04 00:39:25 -0500

    add working SD card test on Heltec

--- esp32/SdCardModule/SdCardModule.ino
+++ esp32/SdCardModule/SdCardModule.ino
@@ -1,19 +1,28 @@
 #include <SPI.h>
 #include <SD.h>
 
-const int CS_PIN = 5;
+#define SD_CS 47
+#define SD_SCK 35
+#define SD_MOSI 34
+#define SD_MISO 33
+
+SPIClass sdSPI(FSPI);
+bool sdReady = false;
 void setup() {
   Serial.begin(115200);
+  delay(3000);
   //error handler for 
   //No SD card inserted,
   //Wrong wiring,
   //Bad CS pin,Corrupted card, 
   //or SPI communication failure
-  if(!SD.begin(CS_PIN)){
+  sdSPI.begin(SD_SCK,SD_MISO,SD_MOSI,SD_CS);
+  if(!SD.begin(SD_CS, sdSPI)){
     Serial.println("SD Card Initialization Failed!");
     return;
   }
   Serial.println("SD Card Ready.");
+  sdReady = true;
   SD.remove("/telemetryData.csv");
   File file = SD.open("/telemetryData.csv", FILE_WRITE);
   
